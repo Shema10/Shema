2b131e9adefb107de3cabcd62b4654e88bbb7dde
Eldin <eldin.semovic@gmail.com>
2017-12-10 17:15:26 +0100

    Project first steps

---
+++ JavaScript/mopjs.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    window.location.href = "file:///C:/Users/Elma/Desktop/MOPProject/Dashboard.html";
+}
+function logout()
+{
+    window.location.href = "file:///C:/Users/Elma/Desktop/MOPProject/MOPHtml.html?#";
+}
+
+function questionnaires()
+{
+    window.location.href = "file:///C:/Users/Elma/Desktop/MOPProject/NekiPage.html";
+}
+function userpage()
+{
+    window.location.href = "file:///C:/Users/Elma/Desktop/MOPProject/UserPage.html#";
+}
+function dashboardMenu()
+{
+    window.location.href = "file:///C:/Users/Elma/Desktop/MOPProject/Dashboard.html#";
+}
